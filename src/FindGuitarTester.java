8975887ca1f87fc8ae14fbd2801619db79ae00f6
Lostgrammer <carlos_cmvp@hotmail.com>
2023-07-21 23:17:23 -0500

    insert guitar, inventory and findguitartestes classes

---
+++ src/FindGuitarTester.java
@@ -0,0 +1,20 @@
+public class FindGuitarTester {
+    public static void main(String[] args) {
+        Inventory inventory = new Inventory();
+        initializeInventory(inventory);
+
+        Guitar searchTest = new Guitar("", "fender", "Stratocastor", "electric", "Alder",
+                                        "Alder",0);
+        Guitar guitar = inventory.search(searchTest);
+        if(guitar!=null){
+            System.out.println("Dear customer, you would like this " +
+                    guitar.getBuilder() + "" + guitar.getModel() + "" +
+                    guitar.getType() + " guitar:\n  " +
+                    guitar.getBackWood() + " back and sides,\n  "+
+                    guitar.getTopWood()+ " top.\nYou can have it for only $"+
+                    guitar.getPrice()+"!");
+        }else {
+            System.out.println("Sorry dear customer, we don't have nothing for you.");
+        }
+    }
+}
---
+++ src/Inventory.java
@@ -0,0 +1,50 @@
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+public class Inventory {
+    private List<Guitar> guitars;
+
+    public Inventory(){
+        guitars = new ArrayList<>();
+    }
+    //adding objects to the list
+    public void addGuittar(String serialNumber,String builder, String model, String type, String backWood, String topWood,
+                           double price){
+        Guitar guitar = new Guitar(serialNumber, builder, model, type, backWood, topWood, price);
+        guitars.add(guitar);
+    }
+
+    public Guitar getGuittar(String serialNumber){
+        for(Guitar guitar:guitars){
+            if (guitar.getSerialNumber().equals(serialNumber)){
+                return guitar;
+            }
+        }
+        return null;
+    }
+
+    public Guitar search(Guitar searchGuitar){
+        for(Guitar guitar:guitars){
+            //ignora serialnumber y price porque son unicos
+            String builder = searchGuitar.getBuilder();
+            String model = searchGuitar.getModel();
+            String type = searchGuitar.getType();
+            String backWood = searchGuitar.getBackWood();
+            String topWood = searchGuitar.getTopWood();
+            if (builder != null && !builder.equals("") && guitar.getBuilder().equals(builder)){
+                continue;
+            }
+            if (type != null && !type.equals("") && guitar.getType().equals(type)){
+                continue;
+            }
+            if (backWood != null && !backWood.equals("") && guitar.getBackWood().equals(backWood)){
+                continue;
+            }
+            if (topWood != null && !topWood.equals("") && guitar.getTopWood().equals(topWood)){
+                continue;
+            }
+        }
+        return null;
+    }
+}
