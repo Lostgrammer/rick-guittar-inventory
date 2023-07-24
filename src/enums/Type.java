bcc84e8b3ab0eea77e25608606bbfc5ff732bf9e
Lostgrammer <carlos_cmvp@hotmail.com>
2023-07-24 11:17:08 -0500

    add Builder, Type, Wood enums

---
+++ src/enums/Type.java
@@ -0,0 +1,17 @@
+package enums;
+
+public enum Type {
+    ACOUSTIC, ELECTRIC;
+
+    public String toString(){
+        switch (this){
+            case ACOUSTIC -> {
+                return "Acoustic";
+            }
+            case  ELECTRIC -> {
+                return "Electric";
+            }
+        }
+        return null;
+    }
+}
