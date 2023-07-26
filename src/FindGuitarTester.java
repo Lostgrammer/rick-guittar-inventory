e8e7af7d3bda9416264330fe8578a1dab553f894
Lostgrammer <carlos_cmvp@hotmail.com>
2023-07-26 18:26:09 -0500

    change object class from Guitar to GuitarSpec and add GuitarSpec objetct into loop for and apply its methods

--- src/FindGuitarTester.java
+++ src/FindGuitarTester.java
@@ -2,27 +2,27 @@ import enums.Builder;
 import enums.Type;
 import enums.Wood;
 
-import java.util.ArrayList;
 import java.util.List;
 
 public class FindGuitarTester {
     public static void main(String[] args) {
         Inventory inventory = new Inventory();
-        initializeInventory(inventory);
+        initializeInventory(inventory); //inicializamos el inventario
 
-        Guitar order1 = new Guitar("", Builder.FENDER, "Stratocastor",
-                Type.ELECTRIC, Wood.ALDER, Wood.ALDER,0);
+        GuitarSpec order1 = new GuitarSpec("Stratocastor", Builder.FENDER,
+                Type.ELECTRIC, Wood.ALDER, Wood.ALDER);
         List<Guitar> resultGuitars;//iniciamos una variable de una lista de objetos
         resultGuitars = inventory.search(order1); //esta variable almacena lista de objetos que es el resultado de aplicar el metodo search al inventario con un objeto como argumento
 
         if(!resultGuitars.isEmpty()){
             System.out.println("Dear customer, you would like these guitars: ");
             for (Guitar guitar:resultGuitars){
-                System.out.println("We have a "+guitar.getBuilder() + " " + guitar.getModel() + " " +
-                guitar.getType() + " guitar:\n  " +
-                guitar.getBackWood() + " back and sides,\n  "+
-                guitar.getTopWood()+ " top.\nYou can have it for only $"+
-                guitar.getPrice()+"!\n ----");
+                GuitarSpec spec = guitar.getSpec(); //el objeto de GuitarSpec contiene sus atributos a traves de .getSpec
+                System.out.println("We have a "+spec.getBuilder() + " " + spec.getModel() + " " +
+                        spec.getType() + " guitar:\n  " +
+                        spec.getBackWood() + " back and sides,\n  "+
+                        spec.getTopWood()+ " top.\nYou can have it for only $"+
+                        guitar.getPrice()+"!\n ----");
             }
         }else {
             System.out.println("Sorry dear customer, we don't have nothing for you.");
@@ -30,9 +30,9 @@ public class FindGuitarTester {
     }
 
     private static void initializeInventory(Inventory inventory){
-        inventory.addGuitar("V95693",Builder.FENDER,"Stratocastor",Type.ELECTRIC,Wood.ALDER,Wood.ALDER,
+        inventory.addGuitar("V95693", Builder.FENDER, "Stratocastor",Type.ELECTRIC,Wood.ALDER,Wood.ALDER,
         1499.95);
-        inventory.addGuitar("V9512",Builder.FENDER,"Stratocastor",Type.ELECTRIC,Wood.ALDER,Wood.ALDER,
+        inventory.addGuitar("V9512", Builder.FENDER, "Stratocastor", Type.ELECTRIC,Wood.ALDER,Wood.ALDER,
                 1499.95);
     }
 }
