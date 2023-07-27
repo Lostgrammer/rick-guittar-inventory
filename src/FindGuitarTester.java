994060f84cb8a5c2909438eb0a18463e0cbfe27f
Lostgrammer <carlos_cmvp@hotmail.com>
2023-07-26 22:03:06 -0500

    change class method initializeInventory addGuitar arguments

--- src/FindGuitarTester.java
+++ src/FindGuitarTester.java
@@ -7,17 +7,17 @@ import java.util.List;
 public class FindGuitarTester {
     public static void main(String[] args) {
         Inventory inventory = new Inventory();
-        initializeInventory(inventory); //inicializamos el inventario
+        initializeInventory(inventory);
 
         GuitarSpec order1 = new GuitarSpec("Stratocastor", Builder.FENDER,
-                Type.ELECTRIC, Wood.ALDER, Wood.ALDER);
-        List<Guitar> resultGuitars;//iniciamos una variable de una lista de objetos
-        resultGuitars = inventory.search(order1); //esta variable almacena lista de objetos que es el resultado de aplicar el metodo search al inventario con un objeto como argumento
+                Type.ELECTRIC, Wood.ALDER, Wood.ALDER, 12);
+        List<Guitar> resultGuitars;
+        resultGuitars = inventory.search(order1);
 
         if(!resultGuitars.isEmpty()){
             System.out.println("Dear customer, you would like these guitars: ");
             for (Guitar guitar:resultGuitars){
-                GuitarSpec spec = guitar.getSpec(); //el objeto de GuitarSpec contiene sus atributos a traves de .getSpec
+                GuitarSpec spec = guitar.getSpec();
                 System.out.println("We have a "+spec.getBuilder() + " " + spec.getModel() + " " +
                         spec.getType() + " guitar:\n  " +
                         spec.getBackWood() + " back and sides,\n  "+
@@ -30,9 +30,10 @@ public class FindGuitarTester {
     }
 
     private static void initializeInventory(Inventory inventory){
-        inventory.addGuitar("V95693", Builder.FENDER, "Stratocastor",Type.ELECTRIC,Wood.ALDER,Wood.ALDER,
-        1499.95);
-        inventory.addGuitar("V9512", Builder.FENDER, "Stratocastor", Type.ELECTRIC,Wood.ALDER,Wood.ALDER,
-                1499.95);
+        //modificando argumentos del metodo addGuitar
+        inventory.addGuitar("V95693",1499.95,
+                new GuitarSpec("Stratocastor",Builder.FENDER,Type.ELECTRIC,Wood.ALDER,Wood.ALDER,12));
+        inventory.addGuitar("V95693",1499.95,
+                new GuitarSpec("Stratocastor",Builder.FENDER,Type.ELECTRIC,Wood.ALDER,Wood.ALDER,12));
     }
 }
