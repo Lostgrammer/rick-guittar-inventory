4d094d2b9cf2fc25af233b413f66076c489a8447
Lostgrammer <carlos_cmvp@hotmail.com>
2023-07-26 22:39:10 -0500

    change search method sending logic to searchGuitar

--- src/Inventory.java
+++ src/Inventory.java
@@ -31,27 +31,9 @@ public class Inventory {
         List<Guitar> guitarResult = new ArrayList<>();
         for(Guitar guitar:guitars){//pasamos por todas las guitarras dentro de la lista guitar
             GuitarSpec guitarSpec = guitar.getSpec();
-            Builder builder = searchGuitar.getBuilder(); //compara todos los elementos(enumeradores y variables) de la lista guitarra con el objeto searchGuitar
-            String model = searchGuitar.getModel().toLowerCase();
-            Type type = searchGuitar.getType();
-            Wood backWood = searchGuitar.getBackWood();
-            Wood topWood = searchGuitar.getTopWood();
-            if (!guitarSpec.getBuilder().equals(builder)){
-                continue;
+            if (guitarSpec.matches(searchGuitar)) {
+                guitarResult.add(guitar);
             }
-            if ((model != null) && (!model.equals("")) && (!model.equals(guitarSpec.getModel().toLowerCase()))){
-                continue;
-            }
-            if (!guitarSpec.getType().equals(type)){
-                continue;
-            }
-            if (!guitarSpec.getBackWood().equals(backWood)){
-                continue;
-            }
-            if (!guitarSpec.getTopWood().equals(topWood)){
-                continue;
-            }
-            guitarResult.add(guitar);
         }
         return guitarResult;
     }
